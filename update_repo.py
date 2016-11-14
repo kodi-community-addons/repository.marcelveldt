a3deeefd4f7951e9beaddbaa1d08a6a6e1bd0696
marcelveldt <m.vanderveldt@outlook.com>
2016-11-14 19:35:16 +0100

    update repo

--- update_repo.py
+++ update_repo.py
@@ -342,6 +342,7 @@ def do_unzip(zip_path, targetdir):
     zip_file = zipfile.ZipFile(zip_path, 'r')
     for fileinfo in zip_file.infolist():
         filename = fileinfo.filename
+        print filename
         cur_path = os.path.join(targetdir, filename)
         if not os.path.isdir(cur_path):
             basedir = os.path.dirname(cur_path)
