6438a55fc2218b17cbc7c165df464764e08df3d7
marcelveldt <m.vanderveldt@outlook.com>
2016-11-14 20:18:36 +0100

    update repo

--- update_repo.py
+++ update_repo.py
@@ -261,12 +261,11 @@ def fetch_addon_from_folder(raw_addon_location, target_folder):
         for (root, dirs, files) in os.walk(addon_location.decode("utf-8")):
             relative_root = os.path.join(
                 addon_metadata.id,
-                os.path.relpath(root, addon_location))
+                os.path.relpath(root, addon_location)).decode("utf-8")
             for relative_path in files:
-                print relative_path
                 archive.write(
-                    os.path.join(root, relative_path).encode("utf-8"),
-                    os.path.join(relative_root, relative_path).encode("utf-8"))
+                    os.path.join(root, relative_path.decode("utf-8")),
+                    os.path.join(relative_root, relative_path.decode("utf-8")))
 
     if not samefile(addon_location, addon_target_folder):
         copy_metadata_files(
