b2f867c8459ef3903a606f686d6418fd1a990f42
marcelveldt <m.vanderveldt@outlook.com>
2016-11-14 18:56:15 +0100

    update repo

--- update_repo.py
+++ update_repo.py
@@ -151,7 +151,7 @@ def fetch_addon_from_git(addon_location, target_folder, temp_folder):
     
     addon_temp = os.path.join(temp_folder,addon_id + alt_addonid)
     with zipfile.ZipFile(
-            temp_file, compression=zipfile.ZIP_DEFLATED) as archive:
+            temp_file, allowZip64=True) as archive:
         # extract zipfile into temp folder
         for file in archive.namelist():
             print file
