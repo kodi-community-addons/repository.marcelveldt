e8b694a03d15ea6bcbb9d9190f05fdc6b83f9b4b
Marcel van der Veldt <m.vanderveldt@outlook.com>
2018-03-30 11:33:15 +0200

    update repo

--- update-directory-structure.sh
+++ update-directory-structure.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+md5_for()
+{
+  if builtin command -v md5 > /dev/null; then
+    echo "$1" | md5
+  elif builtin command -v md5sum > /dev/null ; then
+    echo "$1" | md5sum | awk '{print $1}'
+  else
+    echo "Neither md5 nor md5sum were found in the PATH"
+    return 1
+  fi
+
+  return 0
+}
+
 # Check if we are in the right directory
 ROOT=$(pwd)
 if [[ "$ROOT" != *repository.marcelveldt ]]
@@ -11,19 +26,21 @@ echo "Root Path: ${ROOT}"
 
 # Generate md5 checksum for every .zip file
 for ZIP in $(find ${ROOT} -name '*.zip'); do
-   md5sum ${ZIP} | grep -o '^\S*' > ${ZIP}.md5
+   filename=${ZIP} | grep -o '^\S*' > ${ZIP}.md5
+   md5_for $filename
    echo "Updated ${ZIP}.md5"
 done
 
 # Update addons.xml md5 checksum
-md5sum ${ROOT}/addons.xml | grep -o '^\S*' > ${ROOT}/addons.xml.md5
+filename=${ROOT}/addons.xml | grep -o '^\S*' > ${ROOT}/addons.xml.md5
+md5_for $filename
 echo "Updated ${ROOT}/addons.xml.md5"
 
 # Generate index.html files
 for DIR in $(find ${ROOT} -path ${ROOT}/.git -prune -o -type d); do
   (
   echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-  ls -1pa -I.git "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+  ls "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$"  | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
   echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
   echo "Updated ${DIR}/index.html"
